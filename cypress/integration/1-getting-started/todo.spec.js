2c3b5617ad8e1afc05e081fc5cba200465b72981
Mike DiDomizio <mdidomizio@coveo.com>
2021-11-15 11:22:57 -0500

    Add tests for different states

--- cypress/integration/1-getting-started/todo.spec.js
+++
@@ -1,9 +0,0 @@
-describe('My app', () => {
-  beforeEach((() => {
-    cy.visit('http://localhost:3000/todo-react')
-  }))
-
-  it('should look good', () => {
-    cy.percySnapshot('Initial render of application')
-  });
-});
--- src/__tests__/App.e2e.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-describe('My app', () => {
-    it('should look good', () => {
-        cy.percySnapshot('Initial render of application')
-    });
-});
